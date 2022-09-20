e0ecf53d50691e0211e9c1e1d2b13329ba178f80
Hbyrum2003 <113479844+Hbyrum2003@users.noreply.github.com>
2022-09-20 18:49:18 -0400

    Initial commit

---
+++ src/TheaterKiosk.java
@@ -0,0 +1,10 @@
+public class TheaterKiosk {
+    public static void main(String[] args) {
+        int age;
+        age = 20;
+        if (age >= 21){
+            System.out.println("You get a wrist band!");
+        }
+    }
+
+}
